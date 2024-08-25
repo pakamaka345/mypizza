3ec05a4b88e6314be0d81b6d9cd9cdf83c86b5c1
Nikita.Lysiuk <nikita.lysiuk@skni.umcs.pl>
2024-08-25 23:55:11 +0200

    first day

---
+++ components/shared/products-group-list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+    className?: string;
+}
+
+export const ProductsGroupList: React.FC<Props> = ({ className }) => {
+    return (
+        
+    )
+}
